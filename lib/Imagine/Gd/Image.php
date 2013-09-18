f6daf4769279ee1027ef8d7352b380e402e3396a
Alessio <io.alessio@gmail.com>
2013-09-18 15:03:51 +0200

    remove control for image slice

--- lib/Imagine/Gd/Image.php
+++ lib/Imagine/Gd/Image.php
@@ -145,12 +145,14 @@ final class Image extends AbstractImage
         }
 
         $size = $image->getSize();
-        if (!$this->getSize()->contains($size, $start)) {
-            throw new OutOfBoundsException(
-                'Cannot paste image of the given size at the specified '.
-                'position, as it moves outside of the current image\'s box'
-            );
-        }
+        // Without this condition i can slice original image
+        //
+        //if (!$this->getSize()->contains($size, $start)) {
+        //    throw new OutOfBoundsException(
+        //        'Cannot paste image of the given size at the specified '.
+        //        'position, as it moves outside of the current image\'s box'
+        //    );
+        //}
 
         imagealphablending($this->resource, true);
         imagealphablending($image->resource, true);
